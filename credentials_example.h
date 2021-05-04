378116ee5d43165fdac9c9bf02a982b1064e757f
Charlie Steenhagen <charlie.steenhagen@workiva.com>
2021-05-03 21:52:37 -0400

    Updated credentials example

--- credentials_example.h
+++ credentials_example.h
@@ -3,5 +3,8 @@
 #define RAPID_API_KEY "Your Rapid API Key"
 
 String WARZONE_USERNAME = "DesiredWarzoneUsername";
-String WARZONE_PLATFORM = "psn"; //platform
+String WARZONE_PLATFORM = "psn"; //Platform psn - steam - xbl - battle - uno (Activision ID) - acti (Activision Tag)
 String WARZONE_GET_URL = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/";
+
+//Please note that you'll need to publicize your profile in order to pull data from it on the API
+//https://cod.tracker.gg/cold-war/articles/how-to-public-stats
